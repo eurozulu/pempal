e85c40d352a6d5fc9c6a38f38a8a4228968410f4
Rob Gilham <rob.gilham@loomispay.com>
2023-05-06 20:27:56 +0200

    Added standard (built-in) templates

--- resourceio/resourcetemplates.go
+++ resourceio/resourcetemplates.go
@@ -1,29 +1,101 @@
 package resourceio
 
-import (
-	"pempal/model"
-	"pempal/templates"
-	"strings"
-)
+import "pempal/templates"
+
+var standardTemplates = map[string][]byte{
+	"privatekey":               []byte(PrivateKey),
+	"certificaterequest":       []byte(CertificateRequest),
+	"certificate":              []byte(Certificate),
+	"basic-certificate":        []byte(BasicCertificate),
+	"revokationlist":           []byte(RevokationList),
+	"privatekey-rsa":           []byte(PrivateKeyRSA),
+	"privatekey-ecdsa":         []byte(PrivateKeyECDSA),
+	"certificate-ca":           []byte(CertificateCA),
+	"certificate-intermediate": []byte(CertificateIntermediate),
+	"dn":                       []byte(DN),
+}
+
+const PrivateKey = `
+resource-type: privatekey
+public-key-algorithm: 
+is-encrypted: true
+`
+const CertificateRequest = `
+resource-type: certificaterequest
+subject: null
+signature: 
+signature-algorithm:
+public-key-algorithm:
+public-key:
+version:
+`
+const Certificate = `
+resource-type: certificate
+subject: null
+issuer: null
+signature: 
+signature-algorithm:
+public-key-algorithm:
+public-key:
+version:
+serial-number:
+not-before:
+not-after:
+is-ca: false
+`
+const DN = `common-name:
+country: []
+organization: []
+organizational-unit: []
+locality: []
+province: []
+street-address: []
+postal-code: []
+serial-number:
+`
+
+const BasicCertificate = `
+#extends certificate
+#imports DN "subject"
+#imports DN "issuer"
+
+subject: {{ range $key, $value := .subject }}
+  {{ $key }}: {{ $value }}{{ end }}
+  
+issuer: {{ range $key, $value := .issuer }}
+  {{ $key }}: {{ $value }}{{ end }}
+  
+not-before: {{ now }}
+not-after: {{ nowPlusDays 365 }}
+`
+const RevokationList = `#revokationliste
+`
+
+const PrivateKeyRSA = `#extends privatekey
+public-key-algorithm: RSA
+key-param: 2048
+`
+const PrivateKeyECDSA = `#extends privatekey
+public-key-algorithm: ECDSA
+key-param: P256
+`
+const CertificateCA = `#extends certificate
+is-ca: true
+basic-constraints-valid: true
+max-path-len: -1
+max-path-len-zero: false
+`
+const CertificateIntermediate = `#extends certificate-ca
+max-path-len: 1
+`
 
 func NewResourceTemplateManager(root string) (templates.TemplateManager, error) {
 	tm, err := templates.NewTemplateManager(root)
 	if err != nil {
 		return nil, err
 	}
-	addDefaultForResourceType(model.PublicKey, tm)
-	addDefaultForResourceType(model.PrivateKey, tm)
-	addDefaultForResourceType(model.Certificate, tm)
-	addDefaultForResourceType(model.CertificateRequest, tm)
-	addDefaultForResourceType(model.RevokationList, tm)
-
 	for name, data := range standardTemplates {
-		tm.AddDefaultTemplate(name, data)
+		tm.AddTemplate(name, data)
 	}
 	return tm, nil
-}
-
-func addDefaultForResourceType(r model.ResourceType, tm templates.TemplateManager) {
-	p := strings.Join([]string{"#type", r.String()}, " ")
-	tm.AddDefaultTemplate(r.String(), []byte(p))
 }
